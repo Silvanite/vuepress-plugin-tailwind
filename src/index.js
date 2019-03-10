ccbd4663ec982fe54022cf9fdaf779f155d5540c
Marco Mark <m2de@outlook.com>
2019-03-10 20:59:32 +0000

    Run purge css in production

--- src/index.js
+++ src/index.js
@@ -1,15 +1,65 @@
 import path from 'path'
 
 const plugin = (options = {}, context) => {
-    const { themeConfig, siteConfig } = context
+    const { themeConfig, siteConfig, cwd, isProd } = context
     const { config } = options
 
-    siteConfig.postcss = Object.assign({
-        plugins: [
-            require("tailwindcss")(config || path.join(__dirname, "tailwind.config.js")),
-            require("autoprefixer")
+    const plugins = [
+        require("tailwindcss")(config || path.join(__dirname, "tailwind.config.js")),
+        require("autoprefixer"),
+    ]
+
+    /**
+     * Only run purge css in production.
+     */
+    if (isProd) plugins.push(require("@fullhuman/postcss-purgecss")({
+        content: [
+            `${cwd}/.vuepress/**/*.vue`,
+            `${cwd}/.vuepress/**/*.md`,
+            `${cwd}/.vuepress/**/*.js`,
+            `${cwd}/.vuepress/**/*.html`,
+            `${cwd}/.vuepress/**/*.styl`,
+            "!(node_modules)/**/*.*",
+        ],
+
+        extractors: [
+            {
+                /**
+                 * A fix for purge css to pick up class names with escaped chars
+                 * E.g. md:w-1/2.
+                 *
+                 * Solution from https://github.com/tailwindcss/tailwindcss/issues/391#issuecomment-366922730
+                 */
+                extractor: class TailwindExtractor {
+                    static extract(content) {
+                        return (
+                            content.match(/[A-z0-9-:\/]+/g) || []
+                        );
+                    }
+                },
+                extensions: [
+                    "css",
+                    "html",
+                    "js",
+                    "vue",
+                    "md",
+                    "styl"
+                ]
+            }
+        ],
+
+        /**
+         * Ensure default resets and normalised classes ar enot removed by PurgeCSS
+         */
+        whitelistPatterns: [
+            /^(h\d|p$|ul|li$|div|ol|table|td$|th$|thead|tbody|main|input|button|form|md-|hljs)/
         ]
-    }, siteConfig.postcss)
+    }))
+
+    /**
+     * Merge in the site's purgecss config
+     */
+    siteConfig.postcss = Object.assign(siteConfig.postcss || {}, { plugins })
 
     return {
         name: '@silvanite/vuepress-plugin-tailwind',
